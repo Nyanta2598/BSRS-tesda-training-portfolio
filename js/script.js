02b17f67ab3eccfb597b59c636e48313d7b9ae8f
JoeSteve <pjesteves15@gmail.com>
2022-12-14 00:19:46 +0800

    v1.0.3

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.materialboxed').materialbox();
     $('.scrollspy').scrollSpy();
+    $('.collapsible').collapsible();
 });
 
 function reveal() {
@@ -20,4 +21,54 @@ function reveal() {
   
 window.addEventListener("scroll", reveal);
 
-  
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [
+  "Graphic design", 
+  "Back-End Developer", 
+  "Front-End Developer", 
+  "Mobile Developer", 
+  "Game Developer", 
+  "Photographer",
+];
+
+const typingDelay = 90;
+const erasingDelay = 0;
+const newTextDelay = 400; // Delay between current and next text
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if ( !cursorSpan.classList.contains("typing") ) 
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-200);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
